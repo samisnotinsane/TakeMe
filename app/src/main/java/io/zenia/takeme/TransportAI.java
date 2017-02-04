38c11704875edd51110168582244cfb349ae933a
Tahnik Mustasin <tahnik@live.co.uk>
2017-02-04 12:52:51 +0000

    Updated Android project with new package name

---
+++ app/src/main/java/io/zenia/takeme/TransportAI.java
@@ -0,0 +1,11 @@
+package io.zenia.takeme;
+
+/**
+ * Created by Tahnik Mustasin on 04/02/2017.
+ * This will be the central point of our logic.
+ * Most of our controllers should use this to process information
+ * and get result back from api.ai
+ */
+
+public class TransportAI {
+}
